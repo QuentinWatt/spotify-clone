16bc65798157c51f934117fce7d4d1ec4d2876db
Quentin Watt <watt.quentin@gmail.com>
2023-05-30 19:00:53 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-dark min-h-screen">
+      <div className="bg-primary p-3 text-white text-3xl">Hello</div>
     </div>
   );
-}
+};
 
 export default App;
